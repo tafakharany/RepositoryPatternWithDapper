609a21e73780a1cc7579de7a7e7361f36007d055
Taha Elfakharany <31326367+tafakharany@users.noreply.github.com>
2021-02-28 09:29:55 +0200

    added Product model  and Product Repository

---
+++ RepositoryPatternWithDapper/Repository/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using RepositoryPatternWithDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithDapper.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public bool DeleteProduct(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
