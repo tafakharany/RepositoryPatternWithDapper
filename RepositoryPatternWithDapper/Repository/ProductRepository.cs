879f268c5ca0b78cd1e2aa5fa8a4d7d9ac925ecb
Taha Elfakharany <31326367+tafakharany@users.noreply.github.com>
2021-02-28 09:43:19 +0200

    updated the GetProducts Method in  Product Repository Class

--- RepositoryPatternWithDapper/Repository/ProductRepository.cs
+++ RepositoryPatternWithDapper/Repository/ProductRepository.cs
@@ -1,6 +1,10 @@
-﻿using RepositoryPatternWithDapper.Models;
+﻿using Dapper;
+using RepositoryPatternWithDapper.Infrastructure;
+using RepositoryPatternWithDapper.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +20,10 @@ namespace RepositoryPatternWithDapper.Repository
 
         public IEnumerable<Product> GetProducts()
         {
-            throw new NotImplementedException();
+            using IDbConnection db = new SqlConnection(AppConnection.ConnectionString);
+            if (db.State == ConnectionState.Closed)
+                db.Open();
+            return db.Query<Product>("select * from Products", CommandType.Text);
         }
 
         public bool InsertProduct(Product product)
